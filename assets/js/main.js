f33ab7b61a996185f4a32e0ee4178e507dcdbb2a
Bencs Balázs <bencsbalazs@gmail.com>
2025-03-04 16:19:37 +0100

    digitalclock

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,55 @@
+class DigitalClock extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.width = this.getAttribute('width') || '200px';
+        this.height = this.getAttribute('height') || '100px';
+        this.render();
+    }
+
+    connectedCallback() {
+        this.updateTime();
+        this.interval = setInterval(() => this.updateTime(), 1000);
+    }
+
+    disconnectedCallback() {
+        clearInterval(this.interval);
+    }
+
+    updateTime() {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+
+        this.shadowRoot.querySelector('#clock').textContent = `${hours}:${minutes}:${seconds}`;
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `
+            <style>
+            @import url('https://fonts.cdnfonts.com/css/digital-numbers');
+                .clock-container {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    font-size: 2em;
+                    font-family: 'Digital Numbers', sans-serif;
+                    background: black;
+                    color: white;
+                    border-radius: 10px;
+                    width: ${this.width};
+                    height: ${this.height};
+                }
+            </style>
+            <div class="clock-container" id="clock">--:--:--</div>
+        `;
+    }
+}
+
+customElements.define('digital-clock', DigitalClock);
+
+
 /* ---------------------------
    --- Badges webcomponent ---
    --------------------------- */
