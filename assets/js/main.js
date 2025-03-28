150b23c1d594067b8ce516ed7767fd294ce184b6
Bencs Balázs <bencsbalazs@gmail.com>
2025-03-28 16:03:22 +0100

    Clear css

--- assets/js/main.js
+++ assets/js/main.js
@@ -219,46 +219,6 @@ class CredlyBadgeList extends HTMLElement {
         });
     }
 
-    new Swiper('.portfolio-details-slider', {
-        speed: 400,
-        loop: true,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true,
-        },
-    });
-
-    new Swiper('.testimonials-slider', {
-        speed: 600,
-        loop: true,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false,
-        },
-        slidesPerView: 'auto',
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true,
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 20,
-            },
-
-            1200: {
-                slidesPerView: 3,
-                spaceBetween: 20,
-            },
-        },
-    });
-
     /* ------------------------
        --- Matrix animation ---
        ------------------------ */
